90d0320dad067f7db19d6c9d6ddd5289ebe448aa
itsalexito <yodorte.ab2@gmail.com>
2025-02-13 10:22:18 +0100

    Fix:Typo cerrado2025

--- components/PedidosCerrados.js
+++ components/PedidosCerrados.js
@@ -1,8 +1,9 @@
 const PedidosCerrados =()=>{
     return(
-        <div className='home'>
+        <div className='cerrado'>
         <h2>¡Muchas gracias a todos!</h2>
         <h3>Ya hemos recibido todos vuestros pedidos. Recordad venir a clase los dias 14 y 17 de febrero, quizás os llevéis alguna sorpresa.</h3>
+        <h3>¿No has podido venir esos dias? Ven a Eurielec A208-L para recogerla antes del 21 de febrero. </h3>
         <p>Si tienes alguna duda, contacta con nosotros en <a href="mailto:it@eestec.es"> eurielec.eestec@gmail.com</a> o baja al club (A208-L).</p>
         </div>
         );
